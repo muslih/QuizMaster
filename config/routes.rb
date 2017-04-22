d96e24507076505e6a672fb9e3a465f034d78295
Muslih <muslihzarth@gmail.com>
2017-04-22 15:33:11 +0800

    include bootstrap etc to scss and js

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  get 'session/new'
+
+  get 'session/create'
+
+  get 'session/destroy'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end

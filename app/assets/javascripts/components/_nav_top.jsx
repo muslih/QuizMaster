34a96aa84a19e68e525ec373b316362c7543b81c
Muslih <muslihzarth@gmail.com>
2017-04-26 19:44:42 +0800

    fix counter bottom

--- app/assets/javascripts/components/_nav_top.jsx
+++ app/assets/javascripts/components/_nav_top.jsx
@@ -4,7 +4,7 @@ var NavTop = React.createClass({
 			<div id="navtop">
 	      <div className="container">
 	        <div className="col-sm-12">           
-	          Implementation of Quippers coding test.
+	          Implementation of Quipper&#39;s coding test.
 	          <div className="pull-right">
 	          	Welcome <strong>{this.props.username} </strong> | <a href={this.props.signout} data-method="delete"> Signout </a>
 	          </div>       
